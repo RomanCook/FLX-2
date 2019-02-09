eda218bcfb98b2df3edf08b98faf262fefeebc0d
Marko Maliuk <43031116+marcelo3313@users.noreply.github.com>
2019-02-09 20:14:48 +0200

    Add files via upload

---
+++ FLX_homework_8/FLX_homework_8/homework/js/isSmaller.js
@@ -0,0 +1,5 @@
+function isSmaller (arg1, arg2) {
+	let result = arg1 < arg2;
+	return result;
+}
+isSmaller(5,-1);
---
+++ FLX_homework_8/FLX_homework_8/homework/js/pipe.js
@@ -0,0 +1,17 @@
+var x;
+function pipe () {
+	x = arguments[0];
+	if (arguments.length<2) {
+		return x;
+	} else {
+	for (var i=1; i < arguments.length-1; i++) {
+		arguments[i]();
+		}
+	return arguments[0]+x;
+}
+}
+function addOne () {
+	x+=1;
+	return x;
+}
+pipe(1, addOne, addOne);
