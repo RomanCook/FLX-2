aad3e535f5d84513e18987743ba3101f7fc806ad
Marko Maliuk <43031116+marcelo3313@users.noreply.github.com>
2019-02-24 12:18:15 +0200

    Add files via upload

---
+++ FLX_homework_11/homework/src/app.js
@@ -0,0 +1,100 @@
+let inputValue = document.querySelector('#addNewAction');
+let newActionName = inputValue.value;
+let itemName = document.createTextNode(inputValue.value);
+let addButton = document.querySelector('#addButton');
+let counter = 0;
+let maxList = 10;
+let one = 1;
+
+function checkInput () {
+	inputValue.addEventListener('input', empty=> {
+		if (inputValue.value !== '') {
+			addButton.disabled = false;
+		} else {
+			addButton.disabled = true;
+		}
+	})
+}
+checkInput();
+
+function addItem () {
+	if (inputValue.value === '') {
+		addButton.disabled = true;
+	} else {
+	let newListItem = document.createElement('div');
+	newListItem.className = 'listItem';
+	newListItem.setAttribute('draggable', true);
+    newListItem.setAttribute('ondragover', 'dragOver(ev)');
+    newListItem.setAttribute('ondragstart', 'dragStart(ev)');
+	let newDoneButton = document.createElement('button');
+	newDoneButton.className = 'doneButton';
+	let newIconCheckBox = document.createElement('i');
+	newIconCheckBox.id = 'check_box';
+	newIconCheckBox.className = 'material-icons';
+	newIconCheckBox.innerHTML = 'crop_din';
+	newIconCheckBox.setAttribute('onclick', 'doneItem(this)');
+	let newTextItem = document.createElement('span');
+	newTextItem.innerHTML = inputValue.value;
+	let newDeleteButton = document.createElement('button');
+	newDeleteButton.className = 'deleteButton';
+	newDeleteButton.setAttribute('onclick', 'delItem(this)');
+	newDeleteButton.setAttribute('name', 'delete')
+	let newIconDelete = document.createElement('i');
+	newIconDelete.className = 'material-icons';
+	newIconDelete.innerHTML = 'delete';
+	let todoList = document.getElementById('todoList');
+	todoList.appendChild(newListItem);
+	newListItem.appendChild(newDoneButton);
+	newDoneButton.appendChild(newIconCheckBox);
+	newListItem.appendChild(newTextItem);
+	newListItem.appendChild(newDeleteButton);
+	newDeleteButton.appendChild(newIconDelete);
+	counter++;
+	inputValue.value = '';
+	addButton.disabled = true;
+		if (counter === maxList) {
+			addButton.disabled = true;
+			inputValue.disabled = true;
+			let message = document.getElementById('message');
+			message.classList.remove('showNone');
+		} else {
+			addButton.disabled = false;
+			inputValue.disabled = false;
+			let message = document.getElementById('message');
+			message.classList.add('showNone');
+		}
+	}
+}
+
+function delItem (parent) {
+	parent.parentNode.parentNode.removeChild(parent.parentNode);
+	counter--;
+	if (counter < maxList) {
+		addButton.disabled = false;
+		inputValue.disabled = false;
+		let message = document.getElementById('message');
+		message.classList.add('showNone');
+	} else {
+		addButton.disabled = true;
+		inputValue.disabled = true;
+		message.classList.remove('showNone');
+	}
+}
+
+function doneItem (el) {
+	el.innerHTML = 'check_box';
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function dragStar(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function dragOver(ev) {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
